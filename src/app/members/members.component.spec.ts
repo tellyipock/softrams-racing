629466d73b433320e6e1b7b268ae3ac108b50901
Telly Ipock <ipockzhengh16@ecu.edu>
2021-12-01 22:08:06 -0500

    Updated members specs

--- src/app/members/members.component.spec.ts
+++ src/app/members/members.component.spec.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { By } from '@angular/platform-browser';
 import { MembersComponent } from './members.component';
 
-describe('MembersComponent', () => {
+fdescribe('MembersComponent', () => {
   let component: MembersComponent;
   let fixture: ComponentFixture<MembersComponent>;
 
@@ -20,7 +20,7 @@ describe('MembersComponent', () => {
         {
           provide: Router,
           useClass: class {
-            navigate = jasmine.createSpy('navigate');
+            navigateByUrl = jasmine.createSpy('navigateByUrl');
           }
         }
       ]
@@ -37,19 +37,19 @@ describe('MembersComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('#goToAddMemberForm should route to member-details', inject([Router], (router: Router) => {
-    component.goToAddMemberForm();
+  it('#openMemberDetails should route to member-details', inject([Router], (router: Router) => {
+    component.openMemberDetails('ADD');
     fixture.detectChanges();
-    expect(router.navigate).toHaveBeenCalledWith(['/member-details']);
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/member-details',
+      { state: { action: 'ADD', member: undefined } });
   }));
 
   /* Testing the template */
-  it('addMemberButton should call goToAddMemberForm function when clicked', () => {
-    let btn = fixture.debugElement.query(By.css('#addMemberButton'));
-    spyOn(component, 'goToAddMemberForm');
+  it('add button should call openMemberDetails function when clicked', () => {
+    let btn = fixture.debugElement.query(By.css('#add'));
+    spyOn(component, 'openMemberDetails');
     btn.triggerEventHandler('click', null);
     fixture.detectChanges();
-    expect(component.goToAddMemberForm).toHaveBeenCalled();
-
+    expect(component.openMemberDetails).toHaveBeenCalledWith('ADD');
   });
 });
